1f12105835fb63fd39d34ef4a260488ef57f8a0c
Razafintsialonina Mamy <nyandrianinamamy@gmail.com>
2019-10-23 13:59:57 +0300

    import Response

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Http } from "@angular/http";
+import { Http, Response } from "@angular/http";
 import "rxjs/add/operator/map";
 @Injectable()
 export class ProductService {
@@ -8,6 +8,8 @@ export class ProductService {
   constructor(private _http: Http) {}
 
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map(response => response.json());
+    return this._http
+      .get(this._albumUrl)
+      .map((response: Response) => response.json());
   }
 }
